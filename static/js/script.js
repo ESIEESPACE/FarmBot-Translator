2af60802845df8a3e66ba9b790d96dce46139f3d
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2019-05-02 23:48:46 +0200

    Add selected check

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,23 @@
 $(".td-tr").dblclick(function (event) {
     var target = $(event.target);
+
+    if(target.hasClass("selected") || target.parent().hasClass("selected")) return;
+
     var id = $(event.target).attr("id");
     var content = target.text();
 
+    target.addClass("selected");
+
     target.empty();
-    target.append("<input id=\"tz-" + id + "\" class=\"tz-tr form-control\" value=\"" + content + "\">");
-    $("#tz-" + id).focus();
-    $("#tz-" + id).focusout(function () {
+    target.append("<textarea id=\"tz-" + id + "\" class=\"tz-tr form-control\">" + content + "</textarea>");
+
+    var textarea = $("#tz-" + id);
+
+    textarea.focus();
+    textarea.focusout(function () {
+        target.removeClass("selected");
         target.empty();
-        target.text(content);
-    })
+        target.text(textarea.val());
+    });
+
 });
