763be832e1f0236f0f6a9722f9c50f4a36757fd2
Wallerand Delevacq <wallforfry@gmail.com>
2019-05-02 19:12:19 +0200

    Add Statics

--- farmbot_translator/settings.py
+++ farmbot_translator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sdv99-(r4kk9csm31yj46+ul&qna!eg^uq9ywg7wld#(0*u^!#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
