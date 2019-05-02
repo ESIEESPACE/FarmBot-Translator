8b542330d04ce96470fe3592bdce1f82845f1aed
Wallerand Delevacq <wallforfry@gmail.com>
2019-05-02 19:28:19 +0200

    Add models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import Language, Translation
+
+admin.site.register(Language)
+admin.site.register(Translation)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=50)
+    short = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name+" ("+self.short+")"
+
+
+class Translation(models.Model):
+    position = models.IntegerField()
+    original = models.TextField()
+    translation = models.TextField()
+    translated = models.BooleanField(default=False)
+    language = models.ForeignKey(Language, on_delete=models.SET(None))
+
+    def __str__(self):
+        return self.position+ " : "+self.language
