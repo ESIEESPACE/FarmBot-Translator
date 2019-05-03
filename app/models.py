35198f4c1d71ba2f6ab0efe2adc658990921f008
Wallerand Delevacq <wallforfry@gmail.com>
2019-05-03 15:19:05 +0200

    Add creation and update date to models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Language(models.Model):
     name = models.CharField(max_length=50)
     short = models.CharField(max_length=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name+" ("+self.short+")"
@@ -17,6 +19,8 @@ class Translation(models.Model):
     translated = models.BooleanField(default=False)
     other = models.BooleanField(default=False)
     language = models.ForeignKey(Language, on_delete=models.SET(None))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         if self.translated:
