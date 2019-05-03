a16ceb39dca9b07c888460ecd0c477f7121bddc5
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2019-05-03 08:13:47 +0200

    Change for doubleclick instead of focusoff

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,25 @@
 $(".td-tr").dblclick(function (event) {
-    var target = $(event.target);
 
+    saveTranslation();
+    let target = $(event.target);
+
+    //If it's already selected then return
     if(target.hasClass("selected") || target.parent().hasClass("selected")) return;
 
-
-    var id = $(event.target).attr("id");
-    var content = target.text();
+    let id = $(event.target).attr("id");
+    let content = target.text();
 
     target.addClass("selected");
 
     target.empty();
+
     target.append("<textarea id=\"tz-" + id + "\" class=\"tz-tr form-control\">" + content + "</textarea>");
     target.append("<input id=\"bt-" + id + "\" type='button' value='Cancel'>");
 
-    var textarea = $("#tz-" + id);
-    var cnButton = $("#bt-" + id);
+    let textarea = $("#tz-" + id);
+    let cnButton = $("#bt-" + id);
 
     textarea.focus();
-    textarea.focusout(function () {
-        target.removeClass("selected");
-        target.empty();
-        target.text(textarea.val());
-        $.post( "update/", { language: $.urlParam("language"), id: parseInt(id.substring(3)) + 1, translation: textarea.val() } );
-    });
 
     cnButton.click(function () {
         target.removeClass("selected");
@@ -32,6 +29,23 @@ $(".td-tr").dblclick(function (event) {
 });
 
 $.urlParam = function(name){
-	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	let results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
 	return results[1] || 0;
+};
+
+/**
+ * Save selected translations
+ */
+function saveTranslation() {
+    let selected = $(".selected");
+
+    if(selected.length == 0) return;
+
+    let id = selected.attr("id");
+    let content = selected.find("textarea").val();
+
+    selected.removeClass("selected");
+    selected.empty();
+    selected.text(content);
+    $.post( "update/", { language: $.urlParam("language"), id: parseInt(id.substring(3)) + 1, translation: content } );
 }
